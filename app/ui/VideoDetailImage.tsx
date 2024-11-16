23acb3590b928c7b3661e3de7e21e7338ab12407
spyr0h <jean.pougetoux@outlook.fr>
2024-11-17 00:52:23 +0100

    fixes

--- app/ui/VideoDetailImage.tsx
+++ app/ui/VideoDetailImage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 
 type VideoDetailImageProps = { picture: Picture; index: number };
 
@@ -9,9 +8,9 @@ export default function VideoDetailImage({
   picture,
   index,
 }: VideoDetailImageProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const openPicture = () => {
+    window.open(picture.hostUrl, "_blank"); // Ouvre dans un nouvel onglet
+  };
 
   return (
     <div key={index} className="cursor-pointer">
@@ -19,21 +18,8 @@ export default function VideoDetailImage({
         src={picture.directUrl}
         alt={`Aperçu ${index + 1}`}
         className="w-full h-40 object-cover rounded-lg"
-        onClick={openModal}
+        onClick={openPicture}
       />
-      {isModalOpen && (
-        <dialog id={`img_${index}_modal`} className="modal modal-open">
-          <div className="modal-box w-full flex justify-center items-center text-center">
-            <img
-              src={picture.directUrl}
-              className="max-w-full max-h-full object-contain"
-            />
-          </div>
-          <form method="dialog" className="modal-backdrop">
-            <button onClick={closeModal}>close</button>
-          </form>
-        </dialog>
-      )}
     </div>
   );
 }
